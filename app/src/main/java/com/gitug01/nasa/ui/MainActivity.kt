0950c6a3958af00753190e2417799100cc9c0d93
Alexandr <sasha-03-30@mail.ru>
2022-01-04 17:18:49 +0300

    create Marsfragment

--- app/src/main/java/com/gitug01/nasa/ui/MainActivity.kt
+++ app/src/main/java/com/gitug01/nasa/ui/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.gitug01.nasa.ui
 
 import android.os.Bundle
-import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import android.widget.Toolbar
 import androidx.annotation.IdRes
 import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
@@ -10,25 +13,40 @@ import com.gitug01.nasa.R
 import com.gitug01.nasa.domain.ImageRepo
 import com.gitug01.nasa.domain.app
 import com.gitug01.nasa.domain.entity.ImageEntity
-import kotlinx.coroutines.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 
 class MainActivity : AppCompatActivity(), MainScreenFragment.GettingImageEntity {
 
     private val imageRepo: ImageRepo by lazy { app.imageRepo }
+    private var toolbar: Toolbar? = null
+    private var bottomNavigationMenu: BottomNavigationView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setTheme(R.style.Theme_Moon)
 
+        toolbar = findViewById(R.id.toolbar)
+
         setContentView(R.layout.activity_main)
 
-        replaceFragment(R.id.fragments_container, MainScreenFragment(), false)
+        openFragment(R.id.fragments_container, MainScreenFragment(), false)
 
+        bottomNavigationMenu = findViewById(R.id.bottom_navigation1)
 
+        val a = NavigationBarView.OnItemSelectedListener {
+            Toast.makeText(this, it.title, Toast.LENGTH_SHORT).show()
+            true
+        }
+
+        bottomNavigationMenu!!.setOnItemSelectedListener(a)
     }
 
-    private fun replaceFragment(
+    private fun openFragment(
         @IdRes containerViewId: Int,
         @NonNull fragment: Fragment,
         addToBackStack: Boolean
@@ -39,6 +57,19 @@ class MainActivity : AppCompatActivity(), MainScreenFragment.GettingImageEntity 
             true -> supportFragmentManager.beginTransaction().replace(containerViewId, fragment)
                 .addToBackStack(null).commit()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.today_image -> Toast.makeText(this, item.title, Toast.LENGTH_SHORT).show()
+            R.id.yesterday_image -> Toast.makeText(this, item.title, Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override suspend fun getFilmEntity(): ImageEntity {
